c90b91868d1d4ee5e17038db4f0a02c6153217a9
Debbie <asamoahjaeldeborah@gmail.com>
2022-11-26 20:17:18 +0000

    Added the navData file

---
+++ src/data/navData.js
@@ -0,0 +1,28 @@
+const NavData = [
+    {
+        to: "about",
+        offset: -80,
+        value: "About",
+        duration: 200
+    },
+    {
+        to: "skills",
+        offset: -20,
+        value: "Skills",
+        duration: 200
+    },
+    {
+        to: "projects",
+        offset: -20,
+        value: "Projects",
+        duration: 200
+    },
+    {
+        to: "contact",
+        offset: -20,
+        value: "Contact",
+        duration: 200
+    },
+]
+
+export default NavData
